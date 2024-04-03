45f6e988469a9ae86755e1f18aefd9e2cd27c90e
prakhar12-2 <78843480+prakhar12-2@users.noreply.github.com>
2024-04-03 14:18:12 +0530

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,10 @@ import ProfileView from '../views/ProfileView.vue'
 import FriendsView from '../views/FriendsView.vue'
 import PostView from '../views/PostView.vue'
 import ChatView from '../views/ChatView.vue'
+import TrendView from '../views/TrendView.vue'
+import EditProfileView from '../views/EditProfileView.vue'
+import EditPasswordView from '../views/EditPasswordView.vue'
+import NotificationView from '../views/NotificationView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -51,6 +55,26 @@ const router = createRouter({
       path: '/profile/:id',
       name: 'profile',
       component: ProfileView
+    },
+    {
+      path: '/profile/edit',
+      name: 'editprofile',
+      component: EditProfileView
+    },
+    {
+      path: '/profile/editpassword',
+      name: 'editpassword',
+      component: EditPasswordView
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: NotificationView
+    },
+    {
+      path: '/trend/:id',
+      name: 'trendview',
+      component: TrendView
     },
     {
       path: '/profile/:id/friends',
